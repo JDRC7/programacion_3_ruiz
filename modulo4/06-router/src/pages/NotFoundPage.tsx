da15d32ebff144a45105f1c63737364120d081ec
jdrc7 <jdruiz.c@outlook.es>
2026-07-06 09:01:34 -0500

    clase:06/07

---
+++ modulo4/06-router/src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() { 
+    return (
+        <div>
+        <h1 style={{ color: '#1f2937' }}>Pagina no encontrada</h1>
+        <p style={{ color: '#374151' }}>La pagina que buscas no existe.
+        </p>
+        </div>
+    )
+}
